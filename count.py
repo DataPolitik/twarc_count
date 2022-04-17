ef38a18628601ee454521464175ff06d2bd8f40a
jorgechp <jorgechp@yandex.com>
2022-04-17 18:31:45 +0200

    Preliminary version

--- count.py
+++ count.py
@@ -2,50 +2,66 @@ import json
 import click
 import pandas as pd
 
-from textblob import TextBlob
+from langdetect import detect, DetectorFactory
+
+DetectorFactory.seed = 0
 
 
-def output_value(output_string, file):
+def output_value(output_string, file, is_verbose):
     click.echo(output_string, file=file)
-    click.echo(output_string)
+    if is_verbose:
+        click.echo(output_string)
 
 
 @click.command()
 @click.argument('infile', type=click.File('r'), default='-')
 @click.argument('outfile', type=click.File('w'), default='-')
+@click.option('-v', '--verbose', is_flag=True, flag_value=True, default=False, help='Output information in the terminal')
 @click.option('-t', '--tweets', is_flag=True, flag_value=True, default=False, help='Count tweets')
 @click.option('-u', '--users', is_flag=True, flag_value=True, default=False, help='Count users')
-@click.option('-l', '--language', is_flag=True, flag_value=True, default=False, help='Count languages')
-def count(infile, outfile, tweets, users, languages):
+@click.option('-l', '--languages', is_flag=True, flag_value=True, default=False, help='Count languages')
+@click.option('-d', '--details', is_flag=True, flag_value=True, default=False, help='Get detailed information')
+@click.option('-sa', '--sort-alphabetically', type=click.Choice(['asc', 'desc']),
+              help='If -d option is enabled, sort information.')
+def count(infile, outfile, verbose, tweets, users, languages, details, sort_alphabetically):
     dataset_columns = []
     if users:
         dataset_columns.append('users')
     if languages:
         dataset_columns.append('languages')
 
-    dataframe = pd.DataFrame(columns=dataset_columns)
+    list_of_dicts = []
+    number_of_tweets = 0
 
     for line in infile:
         tweet = json.loads(line)
-        response_dictionary = {}
+        number_of_tweets = number_of_tweets + 1
 
-        if 'users' in dataset_columns:
-            user_id = tweet['user_id']
-            response_dictionary['user'] = user_id
-        if 'languages' in dataset_columns:
-            tweet_language = TextBlob(tweet['text'])
-            response_dictionary['language'] = tweet_language
+        if len(dataset_columns) > 0:
+            response_dictionary = {}
+            if 'users' in dataset_columns:
+                user_id = tweet['author']['id']
+                response_dictionary['users'] = user_id
+            if 'languages' in dataset_columns:
+                tweet_language = detect(tweet['text'])
+                response_dictionary['languages'] = tweet_language
 
-        dataframe.append(response_dictionary)
+            list_of_dicts.append(response_dictionary)
+
+    dataframe = pd.DataFrame(list_of_dicts)
+
+    if tweets:
+        output_string = "{}: {}".format("tweets", number_of_tweets)
+        output_value(output_string, outfile, verbose)
 
     for column in dataset_columns:
         unique_values = pd.unique(dataframe[column])
-        output_string = "{}: {}\n".format(column, len(unique_values))
-        output_value(output_string, outfile)
-
-    if tweets:
-        output_string = "{}: {}\n".format("Tweets", len(dataframe.index))
-        output_value(output_string, outfile)
+        output_string = "{}: {}".format(column, len(unique_values))
+        output_value(output_string, outfile, verbose)
+        if details:
+            if sort_alphabetically:
+                unique_values.sort() if sort_alphabetically == 'asc' else unique_values[::-1].sort()
+            output_value(', '.join(unique_values), outfile, verbose)
 
 
 if __name__ == '__main__':
