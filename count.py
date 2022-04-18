29e929034bc6362c03fa22fca872d8b444f09578
jorgechp <jorgechp@yandex.com>
2022-04-19 00:34:31 +0200

    Improve group by

--- count.py
+++ count.py
@@ -1,5 +1,5 @@
 from statistics import mean, median, stdev
-from typing import Generator
+from typing import Generator, List, Dict
 
 import ijson
 import click
@@ -10,6 +10,12 @@ from langdetect import detect, DetectorFactory
 
 DetectorFactory.seed = 0
 
+
+COLUMNS_OPERATIONS: Dict[str, List[str]] = {
+    'users': ['count'],
+    'length': ['mean', 'min', 'max'],
+    'languages': ['count']
+}
 
 def load_json_file(infile) -> Generator:
     json_file = ijson.items(infile, '', multiple_values=True)
@@ -33,12 +39,14 @@ def output_value(output_string, file, is_verbose):
 @click.command()
 @click.argument('infile', type=click.File('r'), default='-')
 @click.argument('outfile', type=click.File('w'), default='-')
-@click.option('-v', '--verbose', is_flag=True, flag_value=True, default=False, help='Output information in the terminal')
+@click.option('-v', '--verbose', is_flag=True, flag_value=True, default=False,
+              help='Output information in the terminal')
 @click.option('-t', '--tweets', is_flag=True, flag_value=True, default=False, help='Count tweets')
 @click.option('-u', '--users', is_flag=True, flag_value=True, default=False, help='Count users')
 @click.option('-l', '--languages', is_flag=True, flag_value=True, default=False, help='Count languages')
 @click.option('-e', '--length', is_flag=True, flag_value=True, default=False, help='Size of text')
-@click.option('-g', '--group', type=click.Choice(['users', 'languages', 'length']),  multiple=True,
+@click.option('-p', '--pandas', type=click.Choice(['csv', 'json', 'pickle']), help='Exports the pandas dataframe in the specified format')
+@click.option('-g', '--group', type=click.Choice(['users', 'languages', 'length']), multiple=True,
               help='Groups by one field. Shows mean, median and standard deviation per group.')
 @click.option('-d', '--details', is_flag=True, flag_value=True, default=False, help='Get detailed information')
 @click.option('-sa', '--sort-alphabetically', type=click.Choice(['asc', 'desc']),
@@ -52,18 +60,22 @@ def count(infile,
           users,
           languages,
           length,
+          pandas,
           group,
           details,
           sort_alphabetically,
           sort_frequency):
-
-    dataset_columns = []
+    dataset_columns = {}
+    dataset_columns_list = []
     if users:
-        dataset_columns.append('users')
+        dataset_columns['users'] = COLUMNS_OPERATIONS['users']
+        dataset_columns_list.append('users')
     if languages:
-        dataset_columns.append('languages')
+        dataset_columns['languages'] = COLUMNS_OPERATIONS['languages']
+        dataset_columns_list.append('languages')
     if length:
-        dataset_columns.append('length')
+        dataset_columns['length'] = COLUMNS_OPERATIONS['length']
+        dataset_columns_list.append('length')
 
     list_of_dicts = []
     number_of_tweets = 0
@@ -115,14 +127,26 @@ def count(infile,
                 output_value(', '.join(unique_values), outfile, verbose)
 
     if group:
-        stats = [np.size]
-        if length:
-            stats.extend([np.mean, np.median])
-        group_list = list(group)
-        gb = dataframe.groupby(group_list)[group_list[0]].agg(stats).reset_index()
-        output_string = "\n\nGroup by {}".format(group_list)
+        if len(dataset_columns) == 1:
+            dataframe = dataframe.groupby(dataset_columns[0]).size().reset_index()
+        else:
+            group_as_list = list(group)
+            operations_dict = {}
+            for key, operation in dataset_columns.items():
+                operations_dict[key] = operation
+            dataframe = dataframe.groupby(group_as_list).agg(operations_dict).reset_index()
+
+        if pandas:
+            if pandas == 'csv':
+                dataframe.to_csv('dataframe.csv')
+            elif pandas == 'json':
+                dataframe.to_json('dataframe.json')
+            elif pandas == 'pickle':
+                dataframe.to_pickle('dataframe.pickle')
+
+        output_string = "\n\nGroup by {}".format(group)
         output_value(output_string, outfile, verbose)
-        output_string = gb.to_string(index=False)
+        output_string = dataframe.to_string(index=False)
         output_value(output_string, outfile, verbose)
 
 
